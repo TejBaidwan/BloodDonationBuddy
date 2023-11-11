0ce69ca68e00283695852803376d19edf4a353b6
Tej Baidwan <w0806279@myscc.ca>
2023-11-11 01:35:23 -0500

    #14 Find Blood Types Fragment Components to be Populated by ViewPager via newInstance() Modified Blood Types Fragment Class by finding components and populating them via newInstance() method to be used by VP adapter

--- app/src/main/java/com/example/blooddonationbuddy/ViewPager/LearnBloodTypesFragment.java
+++ app/src/main/java/com/example/blooddonationbuddy/ViewPager/LearnBloodTypesFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.blooddonationbuddy.R;
 
@@ -21,10 +23,12 @@ public class LearnBloodTypesFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
-    private String mParam2;
+    private int mParam2;
+    private String mParam3;
 
     public LearnBloodTypesFragment() {
         // Required empty public constructor
@@ -39,11 +43,12 @@ public class LearnBloodTypesFragment extends Fragment {
      * @return A new instance of fragment LearnBloodTypesFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LearnBloodTypesFragment newInstance(String param1, String param2) {
+    public static LearnBloodTypesFragment newInstance(String param1, int param2, String param3) {
         LearnBloodTypesFragment fragment = new LearnBloodTypesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM3, param3);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,7 +58,8 @@ public class LearnBloodTypesFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam2 = getArguments().getInt(ARG_PARAM2);
+            mParam3 = getArguments().getString(ARG_PARAM3);
         }
     }
 
@@ -61,6 +67,18 @@ public class LearnBloodTypesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_learn_blood_types, container, false);
+        View view = inflater.inflate(R.layout.fragment_learn_blood_types, container, false);
+
+        TextView bloodType = view.findViewById(R.id.bloodTypeTitle);
+        ImageView bloodTypeImg = view.findViewById(R.id.bloodTypeImage);
+        TextView bloodTypeDesc = view.findViewById(R.id.bloodTypeDesc);
+
+        if (mParam1 != null && mParam3 != null) {
+            bloodType.setText(mParam1);
+            bloodTypeImg.setImageResource(mParam2);
+            bloodTypeDesc.setText(mParam3);
+        }
+
+        return view;
     }
 }
