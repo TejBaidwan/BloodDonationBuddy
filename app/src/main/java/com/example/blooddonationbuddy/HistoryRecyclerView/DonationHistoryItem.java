6d65420a6e0897db3810ff4ee6eca8ff33799502
Tej Baidwan <w0806279@myscc.ca>
2023-12-02 17:20:47 -0500

    #80 Create View for DonationHistoryPage Finsihed the View layout for the donation history item, and modified the DonationHistoryItem class to add an image parameter

--- app/src/main/java/com/example/blooddonationbuddy/HistoryRecyclerView/DonationHistoryItem.java
+++ app/src/main/java/com/example/blooddonationbuddy/HistoryRecyclerView/DonationHistoryItem.java
@@ -2,10 +2,12 @@ package com.example.blooddonationbuddy.HistoryRecyclerView;
 
 public class DonationHistoryItem {
     private int year;
+    private int connector;
     private int event;
 
-    public DonationHistoryItem(int year, int event) {
+    public DonationHistoryItem(int year, int connector, int event) {
         this.year = year;
+        this.connector = connector;
         this.event = event;
     }
 
@@ -15,6 +17,14 @@ public class DonationHistoryItem {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public int getConnector() {
+        return connector;
+    }
+
+    public void setConnector(int connector) {
+        this.connector = connector;
     }
 
     public int getEvent() {
